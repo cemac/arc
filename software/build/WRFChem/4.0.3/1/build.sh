738886b7aa6c7d5750224cad370200ea635f6abb
helen <h.l.burns@leeds.ac.uk>
2019-11-13 16:48:30 +0000

    :pencil2: fix paths and missing make

--- software/build/WRFChem/4.0.3/1/build.sh
+++ software/build/WRFChem/4.0.3/1/build.sh
@@ -94,6 +94,7 @@ function build_wrf() {
   cd mozbc
   fix_MakeFile
   make clean
+  make
   cd ../megan
   fix_MakeFile
   make clean
@@ -152,7 +153,7 @@ function build_wrf() {
   if [ ! -e ${INSTALL_DIR}/bin/WRFchem ] ; then
       mkdir -p ${INSTALL_DIR}/bin/WRFchem
   fi
-  cp -p WRFChem4.0.3/main/*.exe ${INSTALL_DIR}/bin/WRFchem
+  cp -p WRFChem4.0.3/WRFChem4.0.3/main/*.exe ${INSTALL_DIR}/bin/WRFchem
   cp -p WRFChem4.0.3/megan/megan_bio_emiss ${INSTALL_DIR}/bin/
   cp -p WRFChem4.0.3/anthro_emis/anthro_emis ${INSTALL_DIR}/bin/
   cp -p WRFChem4.0.3/finn/src/fire_emis ${INSTALL_DIR}/bin/
@@ -162,10 +163,10 @@ function build_wrf() {
   if [ ! -e ${INSTALL_DIR}/bin/WRFMeteo ] ; then
       mkdir -p ${INSTALL_DIR}/bin/WRF
   fi
-  cp -p WRFMeteo4.0.3/main/*.exe ${INSTALL_DIR}/bin/WRF
-  cp -p flex/bin/* ${INSTALL_DIR}/bin/
-  cp -p WPS4.0.3/*.exe ${INSTALL_DIR}/bin/
-  cp -p WPS4.0.3/link_grib.csh
+  cp -p WRFChem4.0.3/WRFMeteo4.0.3/main/*.exe ${INSTALL_DIR}/bin/WRF
+  cp -p WRFChem4.0.3/flex/bin/* ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.0.3/WPS4.0.3/*.exe ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.0.3/WPS4.0.3/link_grib.csh ${INSTALL_DIR}/bin/
   ln -sf ${INSTALL_DIR}/bin/WRF/wrf.exe ${INSTALL_DIR}/bin/wrfmeteo.exe
   ln -sf ${INSTALL_DIR}/bin/WRFChem/wrf.exe ${INSTALL_DIR}/bin/wrf.exe
   ln -sf ${INSTALL_DIR}/bin/WRFChem/real.exe ${INSTALL_DIR}/bin/real.exe

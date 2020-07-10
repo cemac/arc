4536a637daa4082f52bec5abe737d9638f26e323
CEMAC Support <cemac-support@leeds.ac.uk>
2020-07-10 12:44:37 +0100

    :hammer_and_wrench: minor modifications to wrfchem builds

--- software/build/WRFChem/4.2/1/move.sh
+++ software/build/WRFChem/4.2/1/move.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -
 #title          : build.sh
-#description    : WRFChem 3.7.1
-# instructions  :
-# Source code   :
-# Register      :
+#description    : WRFChem 4.2
+#instructions   :
+#Source code    :
+#Register       :
 #author         : CEMAC - Helen
-#date           : 20191113
-#updated        : 20200622
+#date           : 2020615
+#updated        : 2020615
 #version        : 1
 #usage          : ./build.sh
 #notes          : Helen following Richard's build exmaples
@@ -19,12 +19,11 @@ SRC_DIR=$(readlink -f $(pwd)/../src)
 APPS_DIR="${CEMAC_DIR}/software/apps"
 # app information:
 APP_NAME='WRFChem'
-APP_VERSION='3.7.1'
+APP_VERSION='4.2'
 # build version:
 BUILD_VERSION='1'
 # top level build dir:
 TOP_BUILD_DIR=$(pwd)
-# compilers for which WRF should be built:
 # compilers for which WRF should be built:
 COMPILER_VERS='intel:17.0.1 intel:18.0.2'
 # mpi libraries for which WRF should be built:
@@ -52,12 +51,12 @@ function fix_MakeFile() {
   sed -i "s|LIBS   = -L\$(NETCDF_DIR)/lib \$(AR_FILES)|LIBS   = -lnetcdf -lnetcdff|g" Makefile
 }
 
-if [ ! -e ${SRC_DIR}/'WRFChem3.7.1.tar.gz' ] ; then
+if [ ! -e ${SRC_DIR}/'WRFChem4.2.tar.gz' ] ; then
   # make src directory:
   mkdir -p ${SRC_DIR}
   echo "WRFchem tar file missing manually downloading"
   # get sources:
-  get_file https://github.com/wrf-model/WRF/archive/v4.1.2.tar.gz
+  get_file https://github.com/wrf-model/WRF/archive/v4.2.tar.gz
   get_file http://www.acom.ucar.edu/wrf-chem/mozbc.tar
   get_file http://www.acom.ucar.edu/wrf-chem/megan_bio_emiss.tar
   get_file https://www.acom.ucar.edu/wrf-chem/wes-coldens.tar
@@ -65,15 +64,10 @@ if [ ! -e ${SRC_DIR}/'WRFChem3.7.1.tar.gz' ] ; then
   get_file http://www.acom.ucar.edu/webt/wrf-chem/processors/EDGAR-HTAP.tgz
   get_file https://www.acom.ucar.edu/wrf-chem/EPA_ANTHRO_EMIS.tgz
   get_file http://www.acom.ucar.edu/wrf-chem/megan.data.tar.gz
+  get_file http://www.ncl.ucar.edu/Download/files/flex.tar.gz
   echo "edit build script for new file sctructure"
   echo "stopping...."
   exit 0
-fi
-
-if [ ! -e ${SRC_DIR}/'flex.tar.gz' ] ; then
-  mkdir -p ${SRC_DIR}
-  # get sources:
-  get_file http://www.ncl.ucar.edu/Download/files/flex.tar.gz
 fi
 
 
@@ -87,100 +81,28 @@ function build_wrf() {
   cd ${BUILD_DIR}
   # I've Placed a Tar file of WRFMeteo, Chem, Preprocessors and WPS
   # In src, if downloaded manually put them in a central WRFChem folder!
-  echo "extracting WRFChem"
-  rm -rf WRFChem3.7.1.tar.gz
-  tar xzf ${SRC_DIR}/WRFChem3.7.1.tar.gz
-  cd WRFChem3.7.1
-  echo "building preprocessors"
-  cd mozbc
-  fix_MakeFile
-  make clean
-  make
-  cd ../megan
-  fix_MakeFile
-  make clean
-  make
-  cd ../wes-coldens/
-  fix_MakeFile
-  make clean
-  make wesely
-  make clean
-  make exo_coldens
-  cd ../anthro_emis/
-  fix_MakeFile
-  make clean
-  make
-  cd ../finn/src
-  fix_MakeFile
-  make clean
-  make
-  echo "configuring and compinging WRFChem"
-  cd ../../WRFChem3.7.1
-  if [ $FC == "ifort" ] ; then
-    echo -e "15\n1" | ./configure
-  else
-    echo -e "34\n1" | ./configure
+  if [ ! -e ${INSTALL_DIR}/bin/WRFChem ] ; then
+      mkdir -p ${INSTALL_DIR}/bin/WRFChem
   fi
-  # KPP sometimes doesn't have a bin folder causing the whole thing to fail
-  if [ ! -e chem/KPP/kpp/kpp-2.1/bin ] ; then
-    mkdir chem/KPP/kpp/kpp-2.1/bin
-  fi
-  # if fails try fixing known bug !?!
-  # sed -i "s|-lfl||g" chem/KPP/kpp/kpp-2.1/src/Makefile
-  if [ $MY_CMP == 'gnu:8.3.0' ] ; then
-    sed -i "s|-DBUILD_RRTMG_FAST=1 \ ||g" configure.wrf
-  fi
-  ./compile em_real >& log.compile_wrf-chem
-  echo "configuring and compinging WPS"
-  cd ../WPS3.7.1
-  export WRF_DIR="../WRFChem3.7.1"
-  ./clean -a
-  if [ $FC == "ifort" ] ; then
-    echo -e "17" | ./configure
-  else
-    echo -e "1" | ./configure
-  fi
-  ./compile >& log.compile_wps
-  cd ../WRFMeteo3.7.1
-  echo "configuring and compinging WRF Meteo"
-  export WRF_CHEM=0    # deselectes the WRF-Chem module
-  ./clean -a
-  if [ $FC == "ifort" ] ; then
-    echo -e "15\n1" | ./configure
-  else
-    echo -e "34\n1" | ./configure
-  fi
-  if [ $MY_CMP == 'gnu:8.3.0' ] ; then
-    sed -i "s|-DBUILD_RRTMG_FAST=1 \ ||g" configure.wrf
-  fi
-  ./compile em_real >& log.compile_wrf-meteo
-  cd ${BUILD_DIR}
-  if [ ! -e ${INSTALL_DIR}/bin ] ; then
-    mkdir -p ${INSTALL_DIR}/bin
-  fi
-  if [ ! -e ${INSTALL_DIR}/bin/WRFchem ] ; then
-      mkdir -p ${INSTALL_DIR}/bin/WRFchem
-  fi
-  cp -p WRFChem3.7.1/WRFChem3.7.1/main/*.exe ${INSTALL_DIR}/bin/WRFchem
-  cp -p WRFChem3.7.1/megan/megan_bio_emiss ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/anthro_emis/anthro_emis ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/finn/src/fire_emis ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/mozbc/mozbc ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/wes-coldens/wesely ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/wes-coldens/exo_coldens ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/WRFChem4.2/main/*.exe ${INSTALL_DIR}/bin/WRFChem
+  cp -p WRFChem4.2/megan/megan_bio_emiss ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/anthro_emis/anthro_emis ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/finn/src/fire_emis ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/mozbc/mozbc ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/wes-coldens/wesely ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/wes-coldens/exo_coldens ${INSTALL_DIR}/bin/
   if [ ! -e ${INSTALL_DIR}/bin/WRF ] ; then
       mkdir -p ${INSTALL_DIR}/bin/WRF
   fi
-  cp -p WRFChem3.7.1/WRFMeteo3.7.1/main/*.exe ${INSTALL_DIR}/bin/WRF
-  cp -p WRFChem3.7.1/flex/bin/* ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/WPS3.7.1/*.exe ${INSTALL_DIR}/bin/
-  cp -p WRFChem3.7.1/WPS3.7.1/link_grib.csh ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/WRFMeteo4.2/main/*.exe ${INSTALL_DIR}/bin/WRF
+  cp -p WRFChem4.2/flex/bin/* ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/WPS-4.2/*.exe ${INSTALL_DIR}/bin/
+  cp -p WRFChem4.2/WPS-4.2/link_grib.csh ${INSTALL_DIR}/bin/
   ln -sf ${INSTALL_DIR}/bin/WRF/wrf.exe ${INSTALL_DIR}/bin/wrfmeteo.exe
   ln -sf ${INSTALL_DIR}/bin/WRFchem/wrf.exe ${INSTALL_DIR}/bin/wrf.exe
   ln -sf ${INSTALL_DIR}/bin/WRFchem/real.exe ${INSTALL_DIR}/bin/real.exe
-  cd ${INSTALL_DIR}
-  for BIX in $(find * -maxdepth 1 \
-                 -type f -name '*')
+  for BIX in $(find ${INSTALL_DIR}/bin/* -maxdepth 1 \
+                 -type f )
     do
       # add hdf5 / netcdf lib directories to rpath if required:
       ldd ${BIX} | grep -q hdf5 >& /dev/null
@@ -196,7 +118,8 @@ function build_wrf() {
           ${BIX}
       fi
     done
-  cd ${BUILD_DIR}
+
+
 }
 
 # loop through compilers and mpi libraries:
@@ -225,7 +148,7 @@ do
     NETCDF=$(nc-config --prefix)
     NETCDF_DIR=$NETCDF
     YACC='/usr/bin/yacc -d'
-    FLEX_LIB_DIR=${BUILD_DIR}'/WRFChem3.7.1/flex/lib'
+    FLEX_LIB_DIR=${BUILD_DIR}'/WRFChem4.2/flex/lib'
     LD_LIBRARY_PATH=$FLEX_LIB_DIR:$LD_LIBRARY_PATH
     JASPERLIB='/usr/lib64'
     JASPERINC='/usr/include'

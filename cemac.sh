0b8d957b542106d5b680cc4106261f714c71c5f1
cemachelen <h.l.burns@leeds.ac.uk>
2022-03-04 10:49:33 +0000

    :pencil2: typo

--- cemac.sh
+++ cemac.sh
@@ -6,9 +6,9 @@ export CEMAC_DIR
 
 # add cemac modules:
 case ":${MODULEPATH}:" in
-  ::)  MODULEPATH='${CEMAC_DIR}/software/modulefiles/apps:${CEMAC_DIR}/software/modulefiles/compilers:${CEMAC_DIR}/software/modulefiles/libraries:${CEMAC_DIR}/software/modulefiles/WRFchem';;
+  ::)  MODULEPATH='${CEMAC_DIR}/software/modulefiles/apps:${CEMAC_DIR}/software/modulefiles/compilers:${CEMAC_DIR}/software/modulefiles/libraries:${CEMAC_DIR}/software/modulefiles/WRFChem';;
   *:${CEMAC_DIR}/software/modulefiles/apps:*) :;;
-  *)  MODULEPATH="${CEMAC_DIR}/software/modulefiles/apps:${CEMAC_DIR}/software/modulefiles/compilers:${CEMAC_DIR}/software/modulefiles/libraries:${CEMAC_DIR}/software/modulefiles/WRFchem:${MODULEPATH}";;
+  *)  MODULEPATH="${CEMAC_DIR}/software/modulefiles/apps:${CEMAC_DIR}/software/modulefiles/compilers:${CEMAC_DIR}/software/modulefiles/libraries:${CEMAC_DIR}/software/modulefiles/WRFChem:${MODULEPATH}";;
 esac
 
 # have to unset this to use flavours not owned by root:

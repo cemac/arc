cd37964df1207ca6cbc7ea4f04df9106328fa072
Richard Rigby <r.rigby@leeds.ac.uk>
2025-10-17 12:56:40 +0100

    Add files for jasper 4.2.8

--- software/build/build_all.sh
+++ software/build/build_all.sh
@@ -76,6 +76,8 @@ cd ${CEMAC_SOFTWARE}/build/xios/20220707/1 && build
 cd ${CEMAC_SOFTWARE}/build/atlas/3.10.3/2 && build
 # fluidity 4.1.20:
 cd ${CEMAC_SOFTWARE}/build/fluidity/4.1.20/1 && build
+# jasper 4.2.8:
+cd ${CEMAC_SOFTWARE}/build/jasper/4.2.8/1 && build
 
 # update permissions ... :
 echo "*  updating permissions in ${CEMAC_DIR}"

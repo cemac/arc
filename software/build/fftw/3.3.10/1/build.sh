05013a1e1ab3e9be4124c52a12e04dbf026396e7
Richard Rigby <r.rigby@leeds.ac.uk>
2025-10-20 19:17:15 +0100

    More module file adjustment for dependent modules ...
    Adjust modulefiles, so the _LMFILES_ order matches
    LODADEDMODULES files order, otherwise things go funny ...

--- software/build/fftw/3.3.10/1/build.sh
+++ software/build/fftw/3.3.10/1/build.sh
@@ -140,7 +140,6 @@ do
     sed -i "s|XBUILD_VERSIONX|${BUILD_VERSION}|g" ${MODULEFILE}
     sed -i "s|XFLAVOURX|${FLAVOUR}|g" ${MODULEFILE}
     sed -i "s|XPREREQX|${CMP}/${CMP_VER}|g" ${MODULEFILE}
-    sed -i "s|XLOADBASHX||g" ${MODULEFILE}
   fi
 done
 
@@ -234,14 +233,13 @@ do
     if [ ! -e ${MODULEFILE} ] ; then
       echo "installing modulefile for ${COMPILER_VER} and ${MPI_VER}"
       mkdir -p ${MODULEFILES_DIR}/${FLAVOUR}/${APP_NAME}
-      \cp ${SRC_DIR}/modulefile \
+      \cp ${SRC_DIR}/modulefile.mpi \
         ${MODULEFILE}
       sed -i "s|XAPP_NAMEX|${APP_NAME}|g" ${MODULEFILE}
       sed -i "s|XAPP_VERSIONX|${APP_VERSION}|g" ${MODULEFILE}
       sed -i "s|XBUILD_VERSIONX|${BUILD_VERSION}|g" ${MODULEFILE}
       sed -i "s|XFLAVOURX|${FLAVOUR}|g" ${MODULEFILE}
       sed -i "s|XPREREQX|${CMP}/${CMP_VER} ${MP}/${MP_VER}|g" ${MODULEFILE}
-      sed -i 's|XLOADBASHX|puts stdout "LOADEDMODULES=\\"\\${LOADEDMODULES/:$module_name\\\\\\\/$module_version/}:$module_name\\/$module_version\\" ; export LOADEDMODULES"|g' ${MODULEFILE}
     fi
   done
 done

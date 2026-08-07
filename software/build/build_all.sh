d2accf460379902d82f081e2e71e58a66e693682
Richard Rigby <r.rigby@leeds.ac.uk>
2026-08-07 07:23:42 +0100

    Add nvhpc 23.11

--- software/build/build_all.sh
+++ software/build/build_all.sh
@@ -30,6 +30,8 @@ cd ${CEMAC_SOFTWARE}/build/tcsh/6.24.15/1 && build
 cd ${CEMAC_SOFTWARE}/build/gnu/14.2.0/1 && build
 # intel 2025.2.0:
 cd ${CEMAC_SOFTWARE}/build/intel/2025.2.0/1 && build
+# nvhpc 23.11:
+cd ${CEMAC_SOFTWARE}/build/nvhpc/23.11/1 && build
 # openmpi 5.0.6:
 cd ${CEMAC_SOFTWARE}/build/openmpi/5.0.6/1 && build
 # mvapich 4.0:

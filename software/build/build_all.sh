c8a35332ae1cca94d026582414585c1218c22f8b
Richard Rigby <r.rigby@leeds.ac.uk>
2026-08-18 18:55:53 +0100

    Update atlas build
    Add nvhpc

--- software/build/build_all.sh
+++ software/build/build_all.sh
@@ -81,7 +81,7 @@ cd ${CEMAC_SOFTWARE}/build/bisicles/gia/20210202/1 && build
 # xios 20220707:
 cd ${CEMAC_SOFTWARE}/build/xios/20220707/2 && build
 # atlas 3.10.3:
-cd ${CEMAC_SOFTWARE}/build/atlas/3.10.3/2 && build
+cd ${CEMAC_SOFTWARE}/build/atlas/3.10.3/3 && build
 # fluidity 4.1.20:
 cd ${CEMAC_SOFTWARE}/build/fluidity/4.1.20/1 && build
 # jasper 4.2.8:
